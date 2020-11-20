4abb27d26127021916b4ba6a0769f09c22e263b6
sovup <sovup@DESKTOP-0GB5QBI>
2020-11-20 17:14:38 +0900

    기초 INDEX 완료 WEARE 완료 WORK작업중

---
+++ src/main/webapp/resources/sample/js/user.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/*
+ * function menu() { var gnb = document.getElementById("gnb");
+ * if(gnb.style.display=='none'){ gnb.style.display="block"; }else{
+ * gnb.style.display="none"; } }
+ */
+$(document).ready(function(){
+$("#gnb").hide();
+$('#menu_toggle_btn').click(function() {
+	$('#gnb').slideToggle('fast', function() {
+		// 객체가 다 펼치지거나 접히고 나면 여기에 든 내용이 실행된다.
+	});
+});
+});
+
+$(document).ready(function(){
+	$('#gnb li').addClass('out');
+	
+	   $('#gnb li').mouseover(function(){
+	      $(this).addClass('hover')
+	   });
+	   $('#gnb li').mouseout(function(){
+	      $(this).removeClass('hover');
+	   });
+	});
