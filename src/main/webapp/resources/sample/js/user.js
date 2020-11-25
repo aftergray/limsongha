e1d11ecbd79e909611fba9d76a2d7ce4e689d527
sovup <sovup@DESKTOP-0GB5QBI>
2020-11-25 13:22:17 +0900

    커밋

--- src/main/webapp/resources/sample/js/user.js
+++ src/main/webapp/resources/sample/js/user.js
@@ -7,22 +7,23 @@
  * if(gnb.style.display=='none'){ gnb.style.display="block"; }else{
  * gnb.style.display="none"; } }
  */
-$(document).ready(function(){
-$("#gnb").hide();
-$('#menu_toggle_btn').click(function() {
-	$('#gnb').slideToggle('fast', function() {
-		// 객체가 다 펼치지거나 접히고 나면 여기에 든 내용이 실행된다.
+$(document).ready(function() {
+	$('#menu_toggle_btn').click(function() {
+		$('#gnb').slideToggle('fast', function() {
+			// 객체가 다 펼치지거나 접히고 나면 여기에 든 내용이 실행된다.
+		});
 	});
-});
 });
 
-$(document).ready(function(){
-	$('#gnb li').addClass('out');
+/*
+$(document).ready(function() {
 	
-	   $('#gnb li').mouseover(function(){
-	      $(this).addClass('hover')
-	   });
-	   $('#gnb li').mouseout(function(){
-	      $(this).removeClass('hover');
-	   });
+	$('#gnb li').addClass('out1');
+	$('#gnb li').mouseover(function() {
+		$(this).addClass('hover2');
 	});
+	$('#gnb li').mouseout(function() {
+		$(this).removeClass('hover2');
+	});
+});
+*/
